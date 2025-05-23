80b11e50de7ba5dd44521750d5286c844a685b56
AroojFatima6827 <aroojahmad2003@gmail.com>
2025-05-23 19:32:44 +0500

    Initial commit: Calculator Project

---
+++ script.js
@@ -0,0 +1,7 @@
+const display = document.querySelector(".display");
+
+window.onload = function() {
+    const button = document.querySelector('.divided');
+    button.style.transition = 'all 0.5s ease';
+    button.style.transform = 'scale(1.2)';
+}
